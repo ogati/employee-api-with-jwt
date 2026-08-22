a87df168beb608d433eadf38d707680fc6c9a5a3
leija <leija@Lei>
2026-08-22 14:45:38 -0400

    Initial version

---
+++ src/main/java/ca/gc/aafc/employee/api/EmployeeApiWithJwtApplication.java
@@ -0,0 +1,13 @@
+package ca.gc.aafc.employee.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApiWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApiWithJwtApplication.class, args);
+	}
+
+}
